7ab1f70952c6ab42390cb370858c4358d870eaf2
dhurg <dhurgamsoghir07@gmail.com>
2026-01-29 14:41:35 +0100

    opg 1-10

--- src/firstTorsdag.java
+++ src/firstTorsdag.java
@@ -32,5 +32,91 @@ System.out.println(FinalPrice);
 
 
 
+//opgave 4
+    int guests = 15;
+    int cupcakes = 40;
+    int CupCakesPp = 40/15;
+    int leftover = cupcakes % guests;
+    System.out.println("each person gets " + CupCakesPp + " cupcakes");
+    System.out.println(leftover + " cupcakes left");
+
+
+    //opgave 5
+    StringBuilder sb = new StringBuilder ();
+    String name = "Anna";
+    int age = 25;
+    sb.append("hello, ");
+    sb.append(name);
+    sb.append( "! " );
+    sb.append("you are ");
+    sb.append (age);
+    sb.append(" years old");
+    System.out.println(sb.toString());
+
+
+    //opgave 6
+    int score = 100;
+    System.out.println(score);
+    score += 50;
+    System.out.println(score);
+    score += 25;
+    System.out.println(score);
+
+
+
+
+    //opgave 7
+    int health = 100;
+    System.out.println(health);
+    health -= 15;
+    System.out.println(health);
+    health -=20;
+    System.out.println(health);
+
+
+
+//opgave 8
+    double price = 50;
+    System.out.println(price);
+    price *= 2;
+    System.out.println(price);
+
+
+    //opgave 9
+    int counter = 10;
+    System.out.println(counter);
+    counter ++;
+    System.out.println(counter);
+    counter --;
+    System.out.println(counter);
+
+    //opgave 10
+    double number = 10;
+    System.out.println(number);
+    number += 5;
+    System.out.println(number);
+    number -= 3;
+    System.out.println(number);
+    number ++;
+    System.out.println(number);
+    number --;
+    System.out.println(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
