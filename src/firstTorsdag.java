e9e8cb439c07cafdb59bba1574d65eab4bdc0141
dhurg <dhurgamsoghir07@gmail.com>
2026-01-29 13:09:16 +0100

    opg 1

---
+++ src/firstTorsdag.java
@@ -0,0 +1,12 @@
+void main(){
+        //opgave 1
+double item1  = 25.50;
+double item2  = 15;
+double item3  = 30;
+double subtotal =item1 + item2 + item3;
+double taxRate = 25;
+double withTax = (subtotal* taxRate/100);
+System.out.println(subtotal);
+System.out.println(subtotal+withTax);
+
+}
