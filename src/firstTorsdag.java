8739ed1a617460f7be61839ec0796ba5b5664037
dhurg <dhurgamsoghir07@gmail.com>
2026-01-29 13:25:03 +0100

    opg 2

--- src/firstTorsdag.java
+++ src/firstTorsdag.java
@@ -9,4 +9,15 @@ double withTax = (subtotal* taxRate/100);
 System.out.println(subtotal);
 System.out.println(subtotal+withTax);
 
+
+        //opgave 2
+    double food = 450;
+double drink = 120;
+double tip = 50;
+int pep= 4;
+double total = food+drink+tip;
+double prPerson = total/pep;
+System.out.println(prPerson);
+
+
 }
